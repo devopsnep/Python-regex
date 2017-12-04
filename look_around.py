6cf2ab1167be9b1ec2a66a4ac75383243ae6baf6
urgen sherpa <norbuurgen@gmail.com>
2017-12-05 00:33:59 +0545

    regex look ahead

--- look_around.py
+++ look_around.py
@@ -4,11 +4,16 @@
 #4 negative look behind>>> ?<!
 import re
 # 1 positive look ahead and non capturing group
-string = '''Dec 1.1.1.1 active
-Dec     1.2.3.4    active
-Dec   2.2.2.2   inactive
-Jan 3.3.3.3         active'''
+string = '''Dec 1.1.1.1 nep active
+Dec     1.2.3.4 chi   active
+Dec   2.2.2.2 ger  inactive
+Jan 3.3.3.3    rus     active'''
 # match only if first column starts with 'Dec' and 3rd column is 'active'
-pattern=re.compile('Dec\s+(\S)\s+(?=active)')   # positive look-ahead
+pattern=re.compile('Dec\s+(\S+)\s+(\S+)\s+(?=active)')   # positive look-ahead
 result=re.findall(pattern,string)
+print(result)
+
+#positive look ahead
+string='''i love apple, cherry, and berry'''
+result=re.findall(r'(\w+)(?=\.|,)',string)
 print(result)
